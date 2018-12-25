f10db3fad5ed67364edc11f0f614dcd977bee944
zhenzhen <@2868382033.com>
2018-12-25 18:18:53 +0800

    完成swiper

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,13 @@ var sass = require('gulp-sass');
 
 var server = require('gulp-webserver');
 
+var list = require("./src/mock/data.json");
+
+let url = require("url");
+
+let path = require("path");
+
+let fs = require("fs");
 
 gulp.task('sass', function() {
     return gulp.src('./src/scss/*.scss')
@@ -19,7 +26,18 @@ gulp.task('server', function() {
     return gulp.src('src')
         .pipe(server({
             port: 9090,
-            
+            open: true,
+            middleware: function(req, res, next) {
+                let pathname = url.parse(req.url).pathname;
+                if (pathname == "/api/list") {
+                    res.end(JSON.stringify({ "code": 0, data: list }))
+                } else if (pathname == "/favicon.ico") {
+                    return
+                } else {
+                    pathname = pathname == "/" ? "index.html" : pathname;
+                    res.end(fs.readFileSync(path.join(__dirname, "src", pathname)))
+                }
+            }
         }))
 })
 
--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,46 @@
-var Swiper = new Swiper("#boxWrap", {
-    navigation: {
-        prevEl: "#prev",
-        nextEl: "#next"
-    },
-    pagination: {
-        el: "#round",
-        clickable: true
+$.ajax({
+    url: "/api/list",
+    type: "get",
+    dataType: "json",
+    success: function(res) {
+        if (res.code == 0) {
+            renderList(res.data)
+        }
     }
 })
+
+
+function renderList(data) {
+    var str = "";
+    data.forEach((i) => {
+        //     str += `
+        //         <div class="swiper-slide">`
+        //     str += renderDatas(i.list)
+        //     str += `</div>
+        //    `;
+        console.log(i.list)
+    })
+
+    $("#boxWrap").html(str);
+
+    var Swiper = new Swiper("#boxWrap", {
+        navigation: {
+            prevEl: "#prev",
+            nextEl: "#next"
+        },
+        pagination: {
+            el: "#round",
+            clickable: true
+        }
+    })
+}
+
+
+function renderDatas(data) {
+    return data.map((i) => {
+        return `<li>
+                    <img src="${i.img}" alt="">
+                    <p>${i.title}</p>
+                </li>`
+    }).join("");
+}
